5cb22f6b1f9c80397e1ddf8ba75eb01812172e76
Olivia Sorensen <olsorensen@students.natickps.org>
2022-12-11 12:23:25 -0500

    Initial Commit

---
+++ rhythm visualizer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  rhythm visualizer
+//
+//  Created by Olivia Sorensen on 12/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
